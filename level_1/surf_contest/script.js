afea7feae41d203eef575fcff3c77ef319efcbf2
chinforoso <marcos_felipe1017@outlook.com>
2021-01-22 15:55:48 -0300

    adicionei surfContest e slider_naMao
    avançando nos exercicios

---
+++ level_1/surf_contest/script.js
@@ -0,0 +1,10 @@
+const calcular = document.getElementById('botao');
+
+function surf(){
+    let notas = document.getElementById('txtnotas').value;
+     
+
+}
+
+
+calcular.addEventListener('click', surf);
